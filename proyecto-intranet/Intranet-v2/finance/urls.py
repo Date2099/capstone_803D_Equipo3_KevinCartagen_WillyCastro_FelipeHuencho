43a0a02720935e06c3d40011305c6cee572a6abd
kevin cartagena <kev.cartagena@duocuc.cl>
2025-11-21 18:27:55 -0300

    implementacion de finanzas final

--- proyecto-intranet/Intranet-v2/finance/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path("finanzas/", TemplateView.as_view(template_name="finance/finanzas.html"), name="finanzas"),
-]
